03b31ad98a9f8127f6c54d213a9c30dc788ff3b1
David Andrew Carr <52014240+davidandcarr@users.noreply.github.com>
2021-04-04 16:10:24 -0500

    initial struct

---
+++ README.md
@@ -0,0 +1,1 @@
+# vandcamp_mod16_bdata
---
+++ config.py
@@ -0,0 +1,10 @@
+#Personal pass library
+
+#RDS Database PW
+rds_pw = 'jam!roqua!'
+
+#AMZ review dataset
+reviews = "https://s3.amazonaws.com/amazon-reviews-pds/tsv/amazon_reviews_us_Musical_Instruments_v1_00.tsv.gz"
+
+
+
